92cb33068820f53a855171dc340d614ebad320fa
HYOEUNL <126545445+HEB2105@users.noreply.github.com>
2024-04-20 23:58:37 +0900

    Update and rename 03.SelectionSort.py to 03.Selection Sort.py

--- 03.Selection Sort.py
+++ 03.Selection Sort.py
@@ -17,3 +17,20 @@ def selectionSort(x) :
 x = [ 5, 2, 8, 6, 1, 9, 3, 7]
 selectionSort(x)
 print(x)
+
+
+
+
+# 책에 있는 코드
+def selectionSort(x) :
+    for size in reversed(range(len(x))) :
+        # x의 요소 갯수 만큼 순회
+        max_i = 0
+        for i in range(1, 1+size) :
+            if x[i] > x[max_i] :
+                # 가장 작은 값의 인덱스를 min_i 에 저장
+                max_i = i
+        print(x)
+
+        # 가장 작은 값이 리스트의 size 자리에 위치하도록
+        change(x, max_i, size)
